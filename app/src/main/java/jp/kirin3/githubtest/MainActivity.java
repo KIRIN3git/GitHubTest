2b75ac4932e339a8b9cd4f9269cc52480b3e2b4f
KIRIN3 <scomputer13@gmail.com>
2019-09-23 19:49:17 +0900

    Initial commit

---
+++ app/src/main/java/jp/kirin3/githubtest/MainActivity.java
@@ -0,0 +1,17 @@
+package jp.kirin3.githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.w( "DEBUG_DATA", "first1");
+    }
+}
