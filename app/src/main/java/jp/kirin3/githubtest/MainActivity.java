26b2a4ee5fb7b7dfacbc3e9e2cc6af0be2178caf
KIRIN3 <scomputer13@gmail.com>
2019-09-23 20:01:25 +0900

    developer1

--- app/src/main/java/jp/kirin3/githubtest/MainActivity.java
+++ app/src/main/java/jp/kirin3/githubtest/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.w( "DEBUG_DATA", "first1");
+
+        Log.w( "DEBUG_DATA", "developer1");
     }
 }
